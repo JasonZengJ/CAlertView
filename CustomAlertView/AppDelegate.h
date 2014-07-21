070977816c602857cdf1e5d874a9158215c670fc
曾杰 <jasonzeng@anjuke.com>
2014-07-21 21:27:53 +0800

    Initial Commit

---
+++ CustomAlertView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomAlertView
+//
+//  Created by jason on 7/21/14.
+//  Copyright (c) 2014 jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomAlertView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomAlertView
+//
+//  Created by jason on 7/21/14.
+//  Copyright (c) 2014 jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
